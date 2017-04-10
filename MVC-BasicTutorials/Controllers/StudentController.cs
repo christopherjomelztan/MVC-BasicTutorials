d5b1de7ff0db2c53ad64cfd42cb64b6e7485f99b
christopherjomelztan <christopherjomelztan@gmail.com>
2017-04-10 16:44:28 +0800

    Initial upload
    Initial upload

---
+++ MVC-BasicTutorials/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_BasicTutorials.Controllers
+{
+    public class StudentController : Controller
+    {
+        [ActionName("TestChangeName")]
+        public ActionResult ActionNameSample(string action)
+        {
+            ViewBag.Message = action;
+            return View();
+        }
+
+        public ActionResult Edit(string std)
+        {
+            return View();
+        }
+
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            Models.Student student = new Models.Student();
+            return View(student.FillStudent());
+        }
+
+        public ActionResult RazorSyntax()
+        {
+            return View();
+        }
+
+        public ActionResult HTMLHelpers()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-BasicTutorials/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace MVC_BasicTutorials.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+
+
+        public List<Student> FillStudent()
+        {
+
+
+            SqlConnection sqlConnection = new SqlConnection();
+            string strConString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestDatabase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True";
+            sqlConnection.ConnectionString = strConString;
+            sqlConnection.Open();
+
+            string query = "Select * from Student";
+            SqlCommand sqlCommand = new SqlCommand(query);
+            sqlCommand.Connection = sqlConnection;
+            SqlDataReader dr = sqlCommand.ExecuteReader();
+
+            var studentList = new List<Student>();
+
+            while (dr.Read())
+            {
+                studentList.Add(new Student() { StudentId = dr.GetInt32(dr.GetOrdinal("StudentId")), StudentName = dr["StudentName"].ToString(), Age = dr.GetInt32(dr.GetOrdinal("Age")) });
+            }
+
+            sqlConnection.Close();
+            return studentList;
+        }
+    }
+
+    
+
+}
